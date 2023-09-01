0860405a2a362ab6d406044b09f76453d02f2238
Asha Bikawat <141121441+ashabikawat@users.noreply.github.com>
2023-09-01 13:15:24 +0530

    Updated Create.jsx

--- Create.jsx
+++ Create.jsx
@@ -1,51 +1,57 @@
+import axios from "axios";
 import React, { useState } from "react";
 
 export default function Create() {
-  const [formData, setFormData] = useState({
-    firstname: "",
-    lastname: "",
+  const [user, setUser] = useState({
+    firstName: "",
+    lastName: "",
   });
 
-  function onHandleChange(e) {
+  function handleChange(e) {
     // console.log(e.target.value);
     // console.log(e.target.name);
-    setFormData({
-      ...formData,
+
+    setUser({
+      ...user,
       [e.target.name]: e.target.value,
     });
   }
 
-  const printValues = (e) => {
+  function handleSubmit(e) {
     e.preventDefault();
-    console.log(formData.firstname, formData.lastname);
-  };
+    // console.log(user);
+    axios.post("https://64e76044b0fd9648b78fdeda.mockapi.io/users", user);
+
+    alert("data is submitted");
+    setUser({
+      firstName: "",
+      lastName: "",
+    });
+  }
 
   return (
-    <>
-      <form onSubmit={printValues}>
-        <label htmlFor="fname"> First Name : </label>
-        <input
-          type="text"
-          placeholder="john"
-          name="firstname"
-          value={formData.firstname}
-          onChange={onHandleChange}
-        />
-        <br />
-        <br />
-        <label htmlFor="lname"> Last Name : </label>
-        <input
-          type="text"
-          placeholder="doe"
-          name="lastname"
-          value={formData.lastname}
-          onChange={onHandleChange}
-        />{" "}
-        <br />
-        <br />
-        <button type="submit">Submit</button>
-      </form>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="fname">First Name : </label>
+      <input
+        type="text"
+        placeholder="john"
+        name="firstName"
+        value={user.firstName}
+        onChange={handleChange}
+      />
       <br />
-    </>
+      <br />
+      <label htmlFor="lname">Last Name : </label>
+      <input
+        type="text"
+        placeholder="doe"
+        name="lastName"
+        value={user.lastName}
+        onChange={handleChange}
+      />
+      <br />
+      <br />
+      <button type="submit">Submit</button>
+    </form>
   );
 }
